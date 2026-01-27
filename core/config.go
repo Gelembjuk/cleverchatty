34cd84e07f4b7f27a9a58b70681b89f9650a6b1e
Roman Gelembjuk <1552191+Gelembjuk@users.noreply.github.com>
2026-01-27 07:06:00 +0000

    Prepare for new version

--- core/config.go
+++ core/config.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	ThisAppName                = "CleverChatty"
-	ThisAppVersion             = "0.4.0"
+	ThisAppVersion             = "0.4.2"
 	transportStdio             = "stdio"
 	transportHTTPStreaming     = "http_streaming"
 	transportSSE               = "sse"
